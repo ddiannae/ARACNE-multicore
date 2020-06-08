5fb4ea1b025d99d1296caf2d57865a6451d7a858
JoseZ <josema.genomics@gmail.com>
2020-06-08 13:15:22 -0500

    Update: min changes

--- compile-aracne.sh
+++ compile-aracne.sh
@@ -6,4 +6,8 @@ cd ARACNE
 make clean
 make
 cd ..
-ln -s ../bin ARACNE 
+ln -s ../bin ARACNE
+
+[[ -f "../bin/usage.txt"]] && echo "Finished successfully." && exit 0
+echo "Something was wrong"
+exit 15
