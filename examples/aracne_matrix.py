c3f6d8d79c1a955faa24dfbad940ff12056f58a2
Diana Elisa García Cortés dgarcia@inmegen.edu.mx <dgarcia@inmegen.edu.mx>
2021-08-02 13:47:01 -0500

    Adding example to get a complete matrix

---
+++ examples/aracne_matrix.py
@@ -0,0 +1,16 @@
+import MultiAracne
+import sys, os
+
+if __name__=='__main__':
+    exp_matrix = sys.argv[1]
+    outdir = sys.argv[2]
+    outmatrix = sys.argv[3]
+    procs = int(sys.argv[4])
+
+    if not os.path.exists(outdir):
+        os.mkdir(outdir)
+        
+    ma = MultiAracne(exp_matrix)
+    ma.run_aracne(processes=procs, outdir=outdir, pval=1)
+    ma.join_adj(outdir, outdir+"/matrix.adj")
+    MultiAracne.adj_to_matrix(outdir+"/matrix.adj", outmatrix)
