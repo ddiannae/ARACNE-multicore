9f219e3b00de9bee93807f53274df5d1bcaf8aef
Diana Elisa García Cortés dgarcia@inmegen.edu.mx <dgarcia@inmegen.edu.mx>
2023-07-05 00:32:18 +0000

    Updating arguments in example file

--- examples/aracne_matrix.py
+++ examples/aracne_matrix.py
@@ -1,11 +1,19 @@
 from MultiAracne import Aracne
-import sys, os
+import sys, os, argparse
 
 if __name__=='__main__':
-    exp_matrix = sys.argv[1]
-    outdir = sys.argv[2]
-    outmatrix = sys.argv[3]
-    procs = int(sys.argv[4])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exp', '-e', help="tsv file of expression matrix", type= str)
+    parser.add_argument('--out', '-o', help="output directory for single MI files", type=str)
+    parser.add_argument('--matrix', '-m', help="output file for final mi matrix", type=str, default="matrix.adj")
+    parser.add_argument('--cores', '-c', help="number of cores available", type= int, default=2)
+
+    args=parser.parse_args()
+
+    exp_matrix = args.exp
+    outdir = args.out
+    outmatrix = args.matrix
+    procs = int(args.cores)
 
     if not os.path.exists(outdir):
         os.mkdir(outdir)
