fc112e5df106ebb2ce9a9117bd67917c5ef80f70
Diana Elisa García Cortés dgarcia@inmegen.edu.mx <dgarcia@inmegen.edu.mx>
2023-09-04 23:09:44 -0600

    Upgrading version

--- setup.py
+++ setup.py
@@ -2,15 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
         name="MultiAracne", 
-        version='0.0.1',
+        version='0.0.2',
         author="Diana García-Cortés",
         author_email="diana.gco@gmail.com",
         description="Python package to run a parallel version of Aracne",
         packages=["MultiAracne"],
         install_requires=[
 		'pandas', 
-		'numpy'], # add any additional packages that 
-        # needs to be installed along with your package. Eg: 'caer'
+		'numpy'],
         keywords=[],
         classifiers= [
             "Development Status :: 3 - Alpha",
